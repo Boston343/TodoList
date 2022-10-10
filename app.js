d40cbbabdd7a46a191ed257172b1e15e58ab4c65
Boston343 <branden.austin1@gmail.com>
2022-10-10 18:36:24 -0400

    added styles, and now have two separate lists. One is a work list at /work, and the other is a normal list at /

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-import express from 'express';          // npm install express
-import path from 'path';
-import https from 'https';              // for forming external get requests
-import { fileURLToPath } from 'url';
+import express from "express"; // npm install express
+import path from "path";
+import https from "https"; // for forming external get requests
+import { fileURLToPath } from "url";
 
 const app = express();
-app.set('view engine', 'ejs');          // using EJS
+app.set("view engine", "ejs"); // using EJS
 const port = process.env.PORT || 3000;
-app.use(express.urlencoded({extended: true}));  // this is for parsing data from html form
+app.use(express.urlencoded({ extended: true })); // this is for parsing data from html form
 
 // __dirname is only available with CJS. Since I am using ESM I need to get it another way
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// static items like other js or css files will not load unless you define where the server should 
+// static items like other js or css files will not load unless you define where the server should
 //      start looking for those files.
 app.use(express.static(path.join(__dirname, "/public")));
 
 // global variables
 var items = [];
+var workItems = [];
 
 // -----------------------------------------------------------------------------------
 // ---------------------------------- Listening --------------------------------------
@@ -29,29 +30,41 @@ app.listen(port, () => {
 // -----------------------------------------------------------------------------------
 // --------------------------------- Get Requests ------------------------------------
 // -----------------------------------------------------------------------------------
-app.get('/', (req, res) => {
+// normal page for the day
+app.get("/", (req, res) => {
     console.log("Server is up and running.");
 
     // get current date and format it
-    var today = new Date();
-    var dateOptions = {
+    let today = new Date();
+    let dateOptions = {
         weekday: "long",
         day: "numeric",
-        month: "long"
-    }
-    var day = today.toLocaleDateString("en-US", dateOptions);
+        month: "long",
+    };
+    let day = today.toLocaleDateString("en-US", dateOptions);
 
     // display page
-    res.render("list",  {day: day, items: items});   
-    
+    res.render("list", { listTitle: day, items: items });
+});
+
+// -----------------------------------------------------------------------------------
+// page for a workday
+app.get("/work", (req, res) => {
+    res.render("list", { listTitle: "Work List", items: workItems });
 });
 
 // -----------------------------------------------------------------------------------
 // -------------------------------- Post Requests ------------------------------------
 // -----------------------------------------------------------------------------------
 //  add new item to Todo List
-app.post('/newItem', (req, res) => {
-    items.push(req.body.newItem);
-    console.log("new item: " + req.body.newItem);
-    res.redirect("/");
+app.post("/newItem", (req, res) => {
+    if (req.body.list === "Work List") {
+        workItems.push(req.body.newItem);
+        console.log("new work item: " + req.body.newItem);
+        res.redirect("/work");
+    } else {
+        items.push(req.body.newItem);
+        console.log("new item: " + req.body.newItem);
+        res.redirect("/");
+    }
 });
